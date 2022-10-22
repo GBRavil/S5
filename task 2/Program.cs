b012cbf7edb49342af9ef60cf4dc54d64e39b8f5
Ravil <lllx2@yandex.ru>
2022-10-23 00:36:05 +0300

    2

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -4,10 +4,18 @@ num[0] = Convert.ToDouble(Console.ReadLine());
 num[1] = Convert.ToDouble(Console.ReadLine());
 num[2] = Convert.ToDouble(Console.ReadLine());
 num[3] = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"Координаты фигуры ({num[0]}) ({num[1]}) ({num[2]}) ({num[3]})");
+Str(num);
 Console.WriteLine("Введите коэффициент масштабирования (k) на выбор 2 или 4 или 0,5");
 double k = Convert.ToDouble(Console.ReadLine());
 for(int i = 0; i < num.Length; i++) {
     num[i] = num[i] * k;
 }
-Console.WriteLine($"Координаты фигуры при k = {k} получаем ({num[0]}) ({num[1]}) ({num[2]}) ({num[3]})");
+Console.WriteLine($"При k = {k}");
+Str(num);
+void Str (double [] n) {
+    string r1 = string.Format("{0:f1}", n[0]);
+    string r2 = string.Format("{0:f1}", n[1]);
+    string r3 = string.Format("{0:f1}", n[2]);
+    string r4 = string.Format("{0:f1}", n[3]);
+    Console.WriteLine($"Координаты фигуры ({r1}) ({r2}) ({r3}) ({r4})");
+}
